4025cc5fc8a2bf6b6d08f5a960a58d36d93d3f90
noggimcey <M8R-uwv71d@sogetthis.com>
2012-05-23 23:41:26 +0300

    BoardTest: a couple of unit tests

--- tests/villebasse/BoardTest.java
+++ tests/villebasse/BoardTest.java
@@ -9,95 +9,110 @@ import static org.junit.Assert.*;
 
 public class BoardTest {
 
-    public BoardTest() {
-    }
+	Board board;
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+	@Before
+	public void setUp()
+	{
+		board = new Board(new Piece());
+	}
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+	/**
+	 * Test of asArray method, of class Board.
+	 */
+	@Test
+	public void testAsArray() throws Exception
+	{
+		Piece[][] boardArray;
 
-    @Before
-    public void setUp() {
-    }
+		boardArray = board.asArray();
+		assertEquals(boardArray.length, 1);
+		assertEquals(boardArray[0].length, 1);
+		assertNotNull(boardArray[0][0]);
 
-    @After
-    public void tearDown() {
-    }
+		for (int i = 1; i <= 5; ++i) {
+			board.putPieceAbsolute(0, i, new Piece());
+			board.putPieceAbsolute(i, 0, new Piece());
+		}
 
-    /**
-     * Test of asArray method, of class Board.
-     */
-    @Test
-    public void testAsArray() {
-        System.out.println("asArray");
-        Board instance = null;
-        Piece[][] expResult = null;
-        Piece[][] result = instance.asArray();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+		boardArray = board.asArray();
+		for (int i = 0; i < boardArray.length; ++i)
+			for (int j = 0; j < boardArray[i].length; ++j)
+				if (i == 0 || j == 0)
+					assertNotNull(boardArray[i][j]);
+				else
+					assertNull(boardArray[i][j]);
+	}
 
-    /**
-     * Test of putPieceAbsolute method, of class Board.
-     */
-    @Test
-    public void testPutPieceAbsolute() throws Exception {
-        System.out.println("putPieceAbsolute");
-        int x = 0;
-        int y = 0;
-        Piece piece = null;
-        Board instance = null;
-        instance.putPieceAbsolute(x, y, piece);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of putPieceAbsolute method, of class Board.
+	 */
+	@Test
+	public void testPutPieceAbsolute()
+	{
+		try {
+			board.putPieceAbsolute(0, 0, new Piece());
+			fail("Absolute point (0,0) is always taken.");
+		} catch (Exception e) {
+			// goood
+		}
 
-    /**
-     * Test of putPieceRelative method, of class Board.
-     */
-    @Test
-    public void testPutPieceRelative() throws Exception {
-        System.out.println("putPieceRelative");
-        int x = 0;
-        int y = 0;
-        Piece piece = null;
-        Board instance = null;
-        instance.putPieceRelative(x, y, piece);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+		try {
+			board.putPieceAbsolute(2, 0, new Piece());
+			fail("Absolute point (2,0) should be unreachable.");
+		} catch (Exception e) {
+			// goood
+		}
 
-    /**
-     * Test of height method, of class Board.
-     */
-    @Test
-    public void testHeight() {
-        System.out.println("height");
-        Board instance = null;
-        int expResult = 0;
-        int result = instance.height();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+		for (int i = 1; i <= 10; ++i)
+			try {
+				board.putPieceAbsolute(i, 0, new Piece());
+			} catch (Exception e) {
+				fail("Absolute point ("+i+",0) should be reachable.");
+			}
+	}
 
-    /**
-     * Test of width method, of class Board.
-     */
-    @Test
-    public void testWidth() {
-        System.out.println("width");
-        Board instance = null;
-        int expResult = 0;
-        int result = instance.width();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of putPieceRelative method, of class Board.
+	 */
+	@Test
+	public void testPutPieceRelative()
+	{
+		for (int i = 0; i < 10; ++i)
+			try {
+				board.putPieceRelative(-1, 0, new Piece());
+			} catch (Exception e) {
+				fail("Relative point (-1,0) should always be valid.");
+			}
+	}
 
+	/**
+	 * Test of height method, of class Board.
+	 */
+	@Test
+	public void testHeight() throws Exception
+	{
+		assertEquals(board.height(), 1);
+
+		board.putPieceAbsolute(-1, 0, new Piece());
+		for (int i = 1; i <= 5; ++i)
+			board.putPieceAbsolute(0, i, new Piece());
+
+		assertEquals(board.height(), 6);
+	}
+
+	/**
+	 * Test of width method, of class Board.
+	 */
+	@Test
+	public void testWidth() throws Exception
+	{
+		assertEquals(board.width(), 1);
+
+		board.putPieceAbsolute(0, -1, new Piece());
+		for (int i = 1; i <= 5; ++i)
+			board.putPieceAbsolute(i, 0, new Piece());
+
+		assertEquals(board.width(), 6);
+	}
 }
